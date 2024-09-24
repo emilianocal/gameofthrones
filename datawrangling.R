95584600596af3f6f8a0f59c13bccae0554e67fa
Emiliano Calvo Alcaniz <ecalvoal@arizona.edu>
2024-09-24 10:17:47 -0700

    step 1 of data wrangling: all lowercase

--- datawrangling.R
+++ datawrangling.R
@@ -3,13 +3,13 @@
 
 ## Figure 1
 
-library
-
-
 library(ggplot2)       ## load in ggplot
 library(wesanderson)   ## load in wes anderson for pretty colors
 library(tidyverse)     ## load in tidyverse
 
-read.csv("GameOfThrones.csv")    ## look at the dataset
+got <- read.csv("GameOfThrones.csv") %>%  #rename to got
+  mutate(across(where(is.character), tolower))
+
+View(got)
 
 
