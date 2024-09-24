ce8bac2d987ca672ccbc23aa443c0e8cdc15758a
Emiliano Calvo Alcaniz <ecalvoal@arizona.edu>
2024-09-24 10:00:01 -0700

    added 3 lines

--- datawrangling.R
+++ datawrangling.R
@@ -1,2 +1,15 @@
-read.csv("GameOfThrones.csv")
+##### Game of Thrones Figures
+
+
+## Figure 1
+
+library
+
+
+library(ggplot2)       ## load in ggplot
+library(wesanderson)   ## load in wes anderson for pretty colors
+library(tidyverse)     ## load in tidyverse
+
+read.csv("GameOfThrones.csv")    ## look at the dataset
+
 
