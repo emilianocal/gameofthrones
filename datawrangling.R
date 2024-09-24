917c6881331ca6b64bb1ce4fdf8c15dcd14fd755
Emiliano Calvo Alcaniz <ecalvoal@arizona.edu>
2024-09-24 10:36:39 -0700

    created basic plot but it might be broken

--- datawrangling.R
+++ datawrangling.R
@@ -1,7 +1,7 @@
 ##### Game of Thrones Figures
 
 
-## Figure 1
+## Figure 1  gender vs time onscreen
 
 library(ggplot2)       ## load in ggplot
 library(wesanderson)   ## load in wes anderson for pretty colors
@@ -12,4 +12,10 @@ got <- read.csv("GameOfThrones.csv") %>%  #rename to got
 
 View(got)
 
+
+got %>% filter(time.hrs > -1) %>%
+  ggplot(aes(x = gender, y = time.hrs)) +
+  theme_classic() 
+  
+  
 
